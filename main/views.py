95daf3c3845715b66602d9957afbc78570d3db12
HilaryOkello <hilaryokello091@gmail.com>
2025-07-12 22:47:48 +0300

    feat: add the most requested services to homepage

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth import logout as django_logout
-
+from services.models import Service, ServiceRequest
+from django.db.models import Count
 
 def home(request):
-    return render(request, "main/home.html", {})
-
+    most_requested = Service.objects.annotate(num_requests=Count('servicerequest')).order_by('-num_requests')[:5]
+    return render(request, "main/home.html", {'most_requested': most_requested})
 
 def logout(request):
     django_logout(request)
